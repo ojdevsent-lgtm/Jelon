f9fa5b9e455cc50524233948a6ad25961398a47e
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-09-18 10:14:47 +0100

    Export Jelon wake-word components

--- engine/voice/__init__.py
+++ engine/voice/__init__.py
@@ -1,3 +1,11 @@
 """Local voice interfaces for Jelon."""
 from .pipeline import VoicePipeline
-__all__ = ["VoicePipeline"]
+from .wake_word import WakeWordConfig, WakeWordDetector, TranscriptWakeWordDetector, WakeWordPipeline
+
+__all__ = [
+    "VoicePipeline",
+    "WakeWordConfig",
+    "WakeWordDetector",
+    "TranscriptWakeWordDetector",
+    "WakeWordPipeline",
+]
