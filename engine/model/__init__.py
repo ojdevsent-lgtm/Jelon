98ec00b230da50fcdba04b14f6482bf84bdafed8
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-09-18 10:09:59 +0100

    Expose GGUF local backend

--- engine/model/__init__.py
+++ engine/model/__init__.py
@@ -1,5 +1,6 @@
 """Local model interfaces for Jelon."""
 
 from .local_model import EchoBackend, InferenceBackend, LocalModel
+from .llama_cpp_backend import LlamaCppBackend
 
-__all__ = ["EchoBackend", "InferenceBackend", "LocalModel"]
+__all__ = ["EchoBackend", "InferenceBackend", "LocalModel", "LlamaCppBackend"]
