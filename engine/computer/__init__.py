a8317e5a29cc991fd347779b986a460fddf65954
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-09-18 10:40:26 +0100

    Export Windows agent tool

--- engine/computer/__init__.py
+++ engine/computer/__init__.py
@@ -1,5 +1,6 @@
 """Concrete and injectable computer-control adapters for Jelon."""
 from .tools import ComputerTool
 from .windows import WindowsComputerAdapter
+from .agent_tool import WindowsComputerTool
 
-__all__ = ["ComputerTool", "WindowsComputerAdapter"]
+__all__ = ["ComputerTool", "WindowsComputerAdapter", "WindowsComputerTool"]
