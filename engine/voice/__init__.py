322c9d726a479dc619b610ee44139821ddca3aee
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-09-18 10:25:15 +0100

    Export continuous wake and local STT components

--- engine/voice/__init__.py
+++ engine/voice/__init__.py
@@ -1,6 +1,10 @@
 """Local voice interfaces for Jelon."""
 from .pipeline import VoicePipeline
 from .wake_word import WakeWordConfig, WakeWordDetector, TranscriptWakeWordDetector, WakeWordPipeline
+from .openwakeword import OpenWakeWordDetector
+from .microphone import MicrophoneConfig, MicrophoneStream, ContinuousWakeListener
+from .continuous import JelonVoiceRuntime
+from .local_stt import SilenceCommandRecorder, FasterWhisperSTT, WhisperCommandCapture
 
 __all__ = [
     "VoicePipeline",
@@ -8,4 +12,12 @@ __all__ = [
     "WakeWordDetector",
     "TranscriptWakeWordDetector",
     "WakeWordPipeline",
+    "OpenWakeWordDetector",
+    "MicrophoneConfig",
+    "MicrophoneStream",
+    "ContinuousWakeListener",
+    "JelonVoiceRuntime",
+    "SilenceCommandRecorder",
+    "FasterWhisperSTT",
+    "WhisperCommandCapture",
 ]
