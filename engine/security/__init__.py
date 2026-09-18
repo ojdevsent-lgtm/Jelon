367abc762427f9ce78c5f79b59cdf788eaeb1f74
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-09-18 11:46:29 +0100

    Export Step 7 security controls

--- engine/security/__init__.py
+++ engine/security/__init__.py
@@ -1,3 +1,6 @@
 """Jelon security and autonomy controls."""
 from .policy import AutonomyPolicy
-__all__ = ["AutonomyPolicy"]
+from .desktop_policy import DesktopPolicy, PolicyStore
+from .permission_gate import PermissionGate, AuthorizationResult
+
+__all__ = ["AutonomyPolicy", "DesktopPolicy", "PolicyStore", "PermissionGate", "AuthorizationResult"]
