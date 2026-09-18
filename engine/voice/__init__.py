7252ddeb9c1a1fdd427ae2909ee831d9c40f3fe1
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-09-18 10:40:29 +0100

    Export complete local voice runtime

--- engine/voice/__init__.py
+++ engine/voice/__init__.py
@@ -5,19 +5,12 @@ from .openwakeword import OpenWakeWordDetector
 from .microphone import MicrophoneConfig, MicrophoneStream, ContinuousWakeListener
 from .continuous import JelonVoiceRuntime
 from .local_stt import SilenceCommandRecorder, FasterWhisperSTT, WhisperCommandCapture
+from .runtime_factory import build_voice_runtime
 
 __all__ = [
-    "VoicePipeline",
-    "WakeWordConfig",
-    "WakeWordDetector",
-    "TranscriptWakeWordDetector",
-    "WakeWordPipeline",
-    "OpenWakeWordDetector",
-    "MicrophoneConfig",
-    "MicrophoneStream",
-    "ContinuousWakeListener",
-    "JelonVoiceRuntime",
-    "SilenceCommandRecorder",
-    "FasterWhisperSTT",
-    "WhisperCommandCapture",
+    "VoicePipeline", "WakeWordConfig", "WakeWordDetector",
+    "TranscriptWakeWordDetector", "WakeWordPipeline", "OpenWakeWordDetector",
+    "MicrophoneConfig", "MicrophoneStream", "ContinuousWakeListener",
+    "JelonVoiceRuntime", "SilenceCommandRecorder", "FasterWhisperSTT",
+    "WhisperCommandCapture", "build_voice_runtime",
 ]
